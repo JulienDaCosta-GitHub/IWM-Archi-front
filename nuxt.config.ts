22ceb3b67ccaee21731998684bdbd68e9c5828e4
Julien Da Costa <juliendacosta27@gmail.com>
2022-11-17 12:40:15 +0100

    Rename main page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            title: 'IWM-Archi-front',
+        }
+    },
+    components: {
+        dirs: [
+            {
+                path: '~/src/nuxt/components/',
+                global: true
+            },
+            '~/components'
+        ]
+    },
+    typescript: {
+        strict: true
+    },
+    dir: {
+        pages: './src/nuxt/pages/'
+    }
 })

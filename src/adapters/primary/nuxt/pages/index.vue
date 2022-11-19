5f8b2c4b8e91ad45276f542e61700fd07be9d87b
eliott11 <55191511+eliott11@users.noreply.github.com>
2022-11-19 15:33:16 +0100

    feat/connexion: update

--- src/adapters/primary/nuxt/pages/index.vue
+++ src/adapters/primary/nuxt/pages/index.vue
@@ -1,15 +1,18 @@
+<script lang="ts" setup>
+   import { listClients } from '~~/src/coreLogic/usecases/client-listing/listClients';
+   import { listClientsVM } from '~~/src/coreLogic/usecases/client-listing/listClients';
+   import { ClientGateway } from '~~/src/coreLogic/gateways/ClientGateway';
+
+   const listClientsVM = computed(() => {
+      return listClientsVM().items
+   })
+</script>
+
 <template>
    <div>
       <h1>Hello World !</h1>
-      <div (v-else v-for="(client) in clientsVM" :key="client.id" )>
-
+      <div (v-else v-for="(client) in listClientsVM" :key="client.id" )>
+         {{client.name}}
       </div>
    </div>
 </template>
-
-<!-- <script lang="ts" setup>
-import { listClients } from '../../../../coreLogic/usecases/client-listing/listClients'
-import { listClientsVM } from '~/src/adapters/primary/view-models/list-clients/listClientsViewModel'
-import { clientGateway } from '~/gateway'
-</script> -->
-

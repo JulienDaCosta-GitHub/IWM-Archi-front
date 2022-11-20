11e85fddc52543f7f9353c431de9cae5931fb8c8
Alizée <agillouaye@icloud.com>
2022-11-20 19:38:17 +0100

    Test liste clients

---
+++ src/coreLogic/usecases/client-listing/listClients.specs.ts
@@ -0,0 +1,32 @@
+import { useClientStore } from '~/src/store/client'
+import { createPinia, setActivePinia } from 'pinia'
+import { listClientsVM, ClientsVM } from '~/src/adapters/primary/view-models/client/list-clients/listClientsViewModel'
+
+describe('List clients view model', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  it('should format price', () => {
+    // Given
+    const client1: any = { id: 'abc123', name: 'Edward Newton' }
+    const client2: any = { id: 'def456', name: 'Martin Matin' }
+    const clientStore = useClientStore()
+    clientStore.list([client1, client1])
+    // When
+    const clientsVM = listClientsVM()
+    // Then
+    const expectedClients: ClientsVM = {
+      items: [
+        {
+          id: 'abc123',
+          name: 'Edward Newton',
+        },
+        {
+          id: 'def456',
+          name: 'Martin Matin',
+        }
+      ]
+    }
+    expect(clientsVM).toEqual(expectedClients)
+  })
+})

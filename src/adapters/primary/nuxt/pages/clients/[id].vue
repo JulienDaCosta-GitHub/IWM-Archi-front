ac3d976ce1cf586bcdbf8c7ba73a329492d9d8c1
Julien Da Costa <juliendacosta27@gmail.com>
2022-11-20 20:24:21 +0100

    update

--- src/adapters/primary/nuxt/pages/clients/[id].vue
+++ src/adapters/primary/nuxt/pages/clients/[id].vue
@@ -10,5 +10,36 @@
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { getClient} from "~/src/coreLogic/usecases/get-client/get-client";
+import { listClientsVM } from '~/src/adapters/primary/view-models/client/list-clients/listClientsViewModel'
+import clientGateway from '~/gateway'
+import { useClientStore } from '~/src/store/client'
+import { computed, onMounted } from '#imports'
+
+const route = useRoute()
+
+onMounted(() => {
+  getClient(route.params.id as string, clientGateway)
+})
+
+const clientsVM = computed(() => {
+  console.log('listClientsVM()', listClientsVM().items)
+  return listClientsVM().items
+})
+
+const redirectoToAdd = () => {
+  window.location.href = "/client/addClient"
+}
+
+const redirectoToSee = (id) => {
+  window.location.href = "/clients/" + id
+}
+
+const isLoading = computed(() => {
+  const clientStore = useClientStore()
+  return clientStore.isLoading
+})
+console.log('clientsVM', clientsVM)
+
 </script>
--- src/coreLogic/usecases/get-client/get-client.ts
+++ src/coreLogic/usecases/get-client/get-client.ts
@@ -1,5 +1,6 @@
 import { ClientGateway } from "../../gateways/ClientGateway"
+import { Client } from '~/src/coreLogic/usecases/client-listing/client'
 
-export const getClient = async(id: string, clientGateway: ClientGateway) => {
+export const getClient = async(id: string, clientGateway: ClientGateway): Promise<Client> => {
   return clientGateway.getById(id)
 }
